6e63231a135ccd675c3322615882faaaaf370c59
Prathamesh Shenoy <prathamesh.shenoy28@gmail.com>
2019-10-02 21:53:14 +0530

    Create grades code.py

---
+++ grades code.py
@@ -0,0 +1,59 @@
+a= float(input('please input percentage marks of comp sci'))
+if a>=90:
+    print('a')
+elif a>=80:
+        print('b')
+elif a>=70:
+        print('c')
+elif a>=60:
+        print('d')
+else:
+    print('fail')
+    
+b= float(input('please input percentage marks of maths'))
+if b>=90:
+    print('a')
+elif b>=80:
+        print('b')
+elif b>=70:
+        print('c')
+elif b>=60:
+        print('d')
+else:
+    print('fail')
+
+c= float(input('please input percentage marks of spanish'))
+if c>=90:
+    print('a')
+elif c>=80:
+        print('b')
+elif c>=70:
+        print('c')
+elif c>=60:
+        print('d')
+else:
+    print('fail')
+
+d= float(input('please input percentage marks of ict'))
+if a>=90:
+    print('a')
+elif d>=80:
+        print('b')
+elif d>=70:
+        print('c')
+elif d>=60:
+        print('d')
+else:
+    print('fail')
+
+e= float(input('please input percentage marks of Economics'))
+if e>=90:
+    print('a')
+elif e>=80:
+        print('b')
+elif e>=70:
+        print('c')
+elif e>=60:
+        print('d')
+else:
+    print('fail')
