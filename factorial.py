5bffeda8f2e5668bd8b72e64ada6f0f3187312a7
Prathamesh Shenoy <prathamesh.shenoy28@gmail.com>
2019-10-04 10:34:50 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,3 +1,9 @@
 import math
-a =math.factorial(int(input('input num')))
-print(f"the factorial is:{a}")
+b = 100
+a = 0
+while b > 0:
+    a=math.factorial(int(input('input num')))
+    
+    print(f"the factorial is: {a}")
+    
+b = b-1   
