cda0886ef0770b9548b2ff2fbc49c71bae9fb2f9
Prathameshshenoy <55439063+Prathameshshenoy@users.noreply.github.com>
2019-10-05 18:41:54 +0530

    Update check how many digits are there in the num(int).py

--- check how many digits are there in the num(int).py
+++ check how many digits are there in the num(int).py
@@ -1,24 +1,6 @@
-a = int(input('please input num'))
-if a<10 and a >-10:
-    print('num has 1 digit')
-elif a<100 and a >-100:
-    print('num has 2 digit')
-elif a<1000 and a >-1000:
-    print('num has 3 digit')
-elif a<10000 and a >-10000:
-    print('num has 4 digit')
-elif a<100000 and a >-100000:
-    print('num has 5 digit')
-elif a<1000000 and a >-1000000:
-    print('num has 6 digit')
-elif a<10000000 and a >-10000000:
-    print('num has 7 digit')
-elif a<100000000 and a >-100000000:
-    print('num has 8 digit')
-elif a<1000000000 and a >-1000000000:
-    print('num has 9 digit')
-elif a<1000000000 and a >-1000000000:
-    print('num has 10 digit')
-else:
-    print('input num in range from 1 -10 digits')
-    
+n=int(input("Enter number:"))
+count=0
+while(n>0):
+    count=count+1
+    n=n//10
+print("The number of digits in the number are:",count)
