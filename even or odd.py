84f5d6ab33779ecaf2052c3c6491203c82bb6f95
Prathameshshenoy <55439063+Prathameshshenoy@users.noreply.github.com>
2019-10-06 16:54:31 +0530

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -1,5 +1,8 @@
-a = float(input('please input num'))
-if a%2==0:
-    print('even')
-else:
+i = 0
+while i<100:
+    a = float(input('please input num'))
+    if a%2==0:
+        print('even')
+    elif a%2 !=0:
     print('odd')
+i = 1+1
